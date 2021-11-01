85ae15c77509fcb4d7f5ed434872f4a28cf59d71
Caleb Moses <calebjdmoses@gmail.com>
2021-11-02 02:20:43 +1300

    Grab text corpus from sentencepiece outputs

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@ set -ex
 
 export SENTENCEPIECE_DIR=../sentencepiece
 
-mkdir ${SENTENCEPIECE_DIR}/models
-cp -r /output/sample_corpus.sentences 
-cp -r /output/* ${SENTENCEPIECE_DIR}/models
+# Copy text corpus from sentencepiece job
+cp -r /sentencepiece-input/corpus ../
 
+# Put sentencepiece model in the right place
+mkdir ${SENTENCEPIECE_DIR}/models
+cp -r /sentencepiece-input/sample_corpus.sentences
+cp -r /sentencepiece-input/models ${SENTENCEPIECE_DIR}
+
+# Do the work
 make pretraining_data
 
+# Save the results
 cp tf_examples.tfrecord
