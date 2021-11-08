f955261860ee8151027b243f45233150e055f65f
Caleb Moses <calebjdmoses@gmail.com>
2021-11-08 18:35:22 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cp -r /input/sentencepiece/corpus ../
 
 # Put sentencepiece model in the right place
 mkdir ${SENTENCEPIECE_DIR}/models
-cp -r /input/sentencepiece/*.sentences ${SENTENCEPIECE_DIR}
 cp -r /input/sentencepiece/models ${SENTENCEPIECE_DIR}
 
 # Do the work
